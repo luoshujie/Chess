ef6f836dd169880860f1d7c910f5554c6bcf6c42
luoshujie <1427339105@qq.com>
2019-10-11 18:17:17 +0800

    添加拖拽逻辑

--- Assets/Script/Scene/Game/UI/HeroCard.cs
+++ Assets/Script/Scene/Game/UI/HeroCard.cs
@@ -1,23 +1,64 @@
 ﻿using Script.Scene.Game.Character.CharacterBase;
 using Script.Scene.Game.Manager;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Script.Scene.Game.UI
 {
-    public class HeroCard : MonoBehaviour
+    public class HeroCard : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHandler
     {
         public Image headImg;
         public Text levelText;
         public Slider energySlider;
         public Slider lifeSlider;
 
+        private PersonData personData = null;
+
+        public Image drayImg;
+        private Transform parentTransf;
+        private Transform targetTransf;
+        private Vector3 startPos;
+        private int layerNum;
+
         public void UpdateData(PersonBase personBase)
         {
-            headImg.sprite = TextureMgr.instance.GetHeroHeadImgAtId(personBase.personData.Id);
-            levelText.text = personBase.personData.Level.ToString();
-            energySlider.value = personBase.personData.CurrEnergy * 1.0f / personBase.personData.MaxEnergy;
-            lifeSlider.value = personBase.personData.CurrLife * 1.0f / personBase.personData.MaxLife;
+            personData = personBase.personData;
+            if (personData == null) return;
+
+            headImg.sprite = TextureMgr.instance.GetHeroHeadImgAtId(personData.Id);
+            drayImg.sprite = headImg.sprite;
+            levelText.text = personData.Level.ToString();
+            energySlider.value = personData.CurrEnergy * 1.0f / personData.MaxEnergy;
+            lifeSlider.value = personData.CurrLife * 1.0f / personData.MaxLife;
+        }
+
+        private void Start()
+        {
+            targetTransf = GameObject.Find("Canvas").transform;
+            parentTransf = drayImg.transform.parent;
+            startPos = drayImg.transform.localPosition;
+            layerNum = drayImg.transform.GetSiblingIndex();
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            Debug.Log("拖拽中...");
+            drayImg.rectTransform.anchoredPosition += eventData.delta;
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            drayImg.transform.SetParent(parentTransf);
+            drayImg.transform.localPosition = startPos;
+            drayImg.transform.SetSiblingIndex(layerNum);
+            Debug.Log("结束拖拽");
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            drayImg.transform.SetParent(targetTransf);
+            Debug.Log("开始拖拽");
         }
     }
 }
